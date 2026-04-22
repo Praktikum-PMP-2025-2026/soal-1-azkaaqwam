68d2ce54c483921fa8566cbd66e211a88c146457
azkaaqwam <azkamhd08@gmail.com>
2026-04-22 09:59:38 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -19,7 +19,7 @@ void AVG(int arr[], int N){
     printf("AVG %.2f\n",avg);
 }
 void MAX(int arr[], int N){
-    int max = 0;
+    int max = arr[0];
     int idx=0;
     for (int i = 0; i < N; i++)
     {
